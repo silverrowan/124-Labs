63f4b7da34b03651b35cca4effe1e6fb456b8ecf
Mariah Malczewska <mariahmalcz@gmail.com>
2026-06-08 12:33:15 -0700

    Refactor StackUsingList to use SinglyLinked n instead of used
    changed to adding to front of list instead of end

--- src/ics124/c0350261/assignment3/StackUsingList.java
+++ src/ics124/c0350261/assignment3/StackUsingList.java
@@ -3,11 +3,11 @@ import ics124.c0350261.assignment2.Ics124SinglyLinkedList;
 
 public class StackUsingList implements Ics124Stack {
     private Ics124SinglyLinkedList data;
-    private int used;
+//    private int used;
 
     public StackUsingList(int capacity) {
         data = new Ics124SinglyLinkedList();
-        used = 0;
+//        used = 0;
     }
 
     public StackUsingList() {
@@ -17,53 +17,41 @@ public class StackUsingList implements Ics124Stack {
     @Override
     public boolean isEmpty() {
         System.out.println("isEmpty Check");
-        if ( used == 0 ) { return true; }
+        if ( data.size() == 0 ) { return true; }
         else { return false; }
     }
 
     @Override
     public boolean isFull() {
-        System.out.println("isFull Check");
-        System.out.println("data.length: " + data.size() + " used: " + used);
         return false;
     }
 
     @Override
     public Integer peek() {
-        if (used == 0) { 
+        if (data.size() == 0) { 
             throw new StackUnderflowException("The stack is empty");
         } else {
-            System.out.println("peek returns: " + data.get(used - 1));
-            return (Integer) data.get(used - 1);
+            return (Integer) data.get(0);
         }
     }
 
     @Override
     public Integer pop() {
-        if (used == 0) { 
+        if (data.size() == 0) { 
             throw new StackUnderflowException("The stack is empty");
         } else {
-//            decrements used then
-//            uses decreased used value to find the final value 
-//            (used starts at 1, index starts at 0 
-            Integer popVal = (Integer) data.get(--used);
-            data.set(used, 0);
-            //sets 'popped' position back to default
-            return popVal;
+            return (Integer) data.remove(0);
         }
     }
 
     @Override
     public void push(Integer a) {
-            data.set(used, a);
-            used ++;
-        }
-//        throw new UnsupportedOperationException("write me!");
-    
+            data.add(0, a);
+        }    
 
     @Override
     public int size() {
-        return used;
+        return data.size();
     }
 
 }
