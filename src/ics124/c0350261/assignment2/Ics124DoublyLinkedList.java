4e3838f77160b37238327a91fdbf0de0c0a07618
Mariah Malczewska <mariahmalcz@gmail.com>
2026-06-08 11:05:14 -0700

    Correct Formatting

--- src/ics124/c0350261/assignment2/Ics124DoublyLinkedList.java
+++ src/ics124/c0350261/assignment2/Ics124DoublyLinkedList.java
@@ -1,14 +1,17 @@
 package ics124.c0350261.assignment2;
+
 import ics124.c0350261.assignment2.Ics124List;
 
 public class Ics124DoublyLinkedList<T> implements Ics124List<T> {
+
     final class DLNode {
+
         T x;
         DLNode prev, next;
     };
     DLNode boundary;
     int n;
-    
+
     final DLNode makeNode(T x) {
         DLNode node = new DLNode();
         node.x = x;
@@ -16,59 +19,66 @@ public class Ics124DoublyLinkedList<T> implements Ics124List<T> {
         node.next = null;
         return node;
     }
-    
+
     public Ics124DoublyLinkedList() {
         boundary = makeNode(null);
         boundary.prev = boundary;
         boundary.next = boundary;
         n = 0;
     }
-    
+
     private DLNode getNode(int i) {
-        if (i < 0 || i >= n) { throw new IndexOutOfBoundsException(); } //i=n doesn't exist
-        else if ( i == n-1 ) { 
-            DLNode current = boundary.prev; 
+        if (i < 0 || i >= n) {
+            throw new IndexOutOfBoundsException();
+        } //i=n doesn't exist
+        else if (i == n - 1) {
+            DLNode current = boundary.prev;
             return current;
-        }
-        else {
+        } else {
             DLNode current = boundary.next;
-            for ( int j = 0 ; j < i ; j++ ) {
+            for (int j = 0; j < i; j++) {
                 current = current.next;
             }
-        return current;
+            return current;
         }
     }
-    
+
     @Override
     public int size() {
         return n;
     }
 
     @Override
-    public T get(int i) { return getNode(i).x; }
+    public T get(int i) {
+        return getNode(i).x;
+    }
 
     @Override
     public void set(int i, T x) {
-        if (i == n) { add(i, x); }
-        else { getNode(i).x = x; }
+        if (i == n) {
+            add(i, x);
+        } else {
+            getNode(i).x = x;
+        }
     }
- 
 
     @Override
     public void add(int i, T x) {
-        if (i < 0 || i > n) { throw new IndexOutOfBoundsException(); } //accepts i = 0 & i = n
+        if (i < 0 || i > n) {
+            throw new IndexOutOfBoundsException();
+        } //accepts i = 0 & i = n
 
         DLNode current = makeNode(x);
         DLNode prevNode;
         DLNode nextNode;
-        if (i == 0) { 
+        if (i == 0) {
             prevNode = boundary;
             nextNode = boundary.next;
         } else if (i == n) {
             prevNode = boundary.prev;
             nextNode = boundary;
         } else {
-            prevNode = getNode(i-1);
+            prevNode = getNode(i - 1);
             nextNode = getNode(i);
         }
         current.prev = prevNode;
@@ -76,23 +86,25 @@ public class Ics124DoublyLinkedList<T> implements Ics124List<T> {
         nextNode.prev = current;
         prevNode.next = current;
         n++;
-    }       
+    }
 
     @Override
     public T remove(int i) {
-        if (i < 0 || i >= n) { throw new IndexOutOfBoundsException(); } //accepts i = 0 & i = n
+        if (i < 0 || i >= n) {
+            throw new IndexOutOfBoundsException();
+        } //accepts i = 0 & i = n
         else {
             DLNode currNode = getNode(i);
 // because maintaining the boundary node the links to the BN should remain - so 
 // doesnt matter which posn being removed, .next & .prev will be correct one
             DLNode prevNode;
-            DLNode nextNode;     
-                prevNode = currNode.prev;
-                nextNode = currNode.next;
+            DLNode nextNode;
+            prevNode = currNode.prev;
+            nextNode = currNode.next;
             prevNode.next = currNode.next;
             nextNode.prev = currNode.prev;
             n--;
-        return currNode.x; //
+            return currNode.x; //
         }
     }
 }
--- src/ics124/c0350261/assignment2/Ics124SinglyLinkedList.java
+++ src/ics124/c0350261/assignment2/Ics124SinglyLinkedList.java
@@ -1,27 +1,30 @@
 package ics124.c0350261.assignment2;
+
 import ics124.c0350261.assignment2.Ics124List;
 
 public class Ics124SinglyLinkedList<T> implements Ics124List<T> {
+
     final class SLNode {
+
         T x;
         SLNode next;
     };
     SLNode head;
     SLNode tail;
     int n;
-    
+
     final SLNode makeNode(T x) {
         SLNode node = new SLNode();
         node.x = x;
         return node;
     }
-    
+
     public Ics124SinglyLinkedList() {
         head = null;
         tail = null;
         n = 0;
     }
-    
+
     @Override
     public int size() {
         return n;
@@ -29,15 +32,16 @@ public class Ics124SinglyLinkedList<T> implements Ics124List<T> {
 
     @Override
     public T get(int i) {
-        if ( i >= n || n == 0 || i < 0) { throw new IndexOutOfBoundsException(); }
-        else {
-         return getNode(i).x; 
+        if (i >= n || n == 0 || i < 0) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            return getNode(i).x;
         }
     }
-    
+
     private SLNode getNode(int i) {
         SLNode current = head;
-        for ( int j = 0 ; j < i ; j++ ) {
+        for (int j = 0; j < i; j++) {
             current = current.next;
         }
         return current;
@@ -45,48 +49,56 @@ public class Ics124SinglyLinkedList<T> implements Ics124List<T> {
 
     @Override
     public void set(int i, T x) {
-        if (i > n || i < 0) { throw new IndexOutOfBoundsException(); }
-        else if (i == n) { add(i, x); }
-        else {
-        SLNode current = getNode(i);
-        current.x = x;
+        if (i > n || i < 0) {
+            throw new IndexOutOfBoundsException();
+        } else if (i == n) {
+            add(i, x);
+        } else {
+            SLNode current = getNode(i);
+            current.x = x;
         }
     }
 
     @Override
     public void add(int i, T x) {
-        if (i > n || i < 0) { throw new IndexOutOfBoundsException(); }
+        if (i > n || i < 0) {
+            throw new IndexOutOfBoundsException();
+        }
         SLNode newNode = makeNode(x);
         if (n == 0) {
             head = tail = newNode;
             //NOT assigning value to next as there is no next to reference
-        } else if (i == 0) { 
+        } else if (i == 0) {
             newNode.next = head;
             head = newNode;
-        } else { 
-            SLNode prevNode = getNode( i-1 );
+        } else {
+            SLNode prevNode = getNode(i - 1);
             newNode.next = prevNode.next;
             prevNode.next = newNode;
-            if (i == n) { tail = newNode; }          
+            if (i == n) {
+                tail = newNode;
+            }
         }
         n++;
     }
 
     @Override
-    public T remove(int i) { 
-        if (i >= n || i < 0) { throw new IndexOutOfBoundsException(); }
+    public T remove(int i) {
+        if (i >= n || i < 0) {
+            throw new IndexOutOfBoundsException();
+        }
         SLNode currNode;
         if (i == 0) {
             currNode = head;
             head = head.next;
-            if(n == 1) {
-                tail = null; 
+            if (n == 1) {
+                tail = null;
             }
         } else {
             //set tail to prevNode, and prevNode.next to Null
             SLNode prevNode = getNode(i - 1);
-            currNode = prevNode.next; 
-            if (i == n-1) {
+            currNode = prevNode.next;
+            if (i == n - 1) {
                 tail = prevNode;
                 prevNode.next = null;
             } else {
