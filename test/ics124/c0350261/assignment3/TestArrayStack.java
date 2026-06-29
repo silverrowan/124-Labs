c9ae40bc2681db71efa853c3925eb9c43ebfa7a3
Mariah Malczewska <mariahmalcz@gmail.com>
2026-06-29 09:00:50 -0700

    Update StackList, tests, remove extra files

--- test/ics124/c0350261/assignment3/TestArrayStack.java
+++ test/ics124/c0350261/assignment3/TestArrayStack.java
@@ -53,25 +53,84 @@ public class TestArrayStack {
 
     @Test
     public void testStackOrder(){
-        StackUsingArray s = new StackUsingArray();
-        
-        s.push(123);
-        s.push(345);
-        s.push(678);
-        s.push(910);
-        
-        assertEquals(910, (int)s.peek());
-        assertEquals(4, s.size());
-        assertEquals(910, (int)s.pop());
-        assertEquals(678, (int)s.peek());
-        assertEquals(678, (int)s.pop());
-        assertEquals(345, (int)s.pop());
-        assertEquals(123, (int)s.peek());
-        assertEquals(1, s.size());
-        
-        assertEquals(123, (int)s.pop());
-        assertEquals(0, s.size());
-        assertThrows(StackUnderflowException.class, () -> s.peek());
-        assertThrows(StackUnderflowException.class, () -> s.pop());
+       StackUsingArray s = new StackUsingArray();
+       s.push(123);
+       s.push(456);
+       s.push(789);
+       
+       assertEquals(3, s.size());
+       assertEquals(789, (int)s.peek());
+       assertEquals(789, (int)s.pop());
+       
+       s.push(159);
+       s.push(48);
+       
+       assertEquals(48, (int)s.peek());
+       assertEquals(48, (int)s.pop());
+       assertEquals(159, (int)s.peek());
+       assertEquals(159, (int)s.pop());
+       assertEquals(456, (int)s.peek());
+       assertEquals(456, (int)s.pop());
+       assertEquals(1, s.size());
+       assertEquals(123, (int)s.peek());
+       assertEquals(123, (int)s.pop());
+       
+       assertEquals(0, s.size());
+       assertThrows(StackUnderflowException.class, () -> s.peek());
+       assertThrows(StackUnderflowException.class, () -> s.pop());
     }
+    
+    @Test
+    public void testSize() {
+       Ics124Stack s = new StackUsingList();
+       assertEquals(0, (int) s.size());
+       s.isEmpty();
+       s.isFull();
+       assertEquals(0, (int) s.size());       
+       s.push(123);
+       s.push(456);
+       s.push(789);
+       assertEquals(3, (int)s.size());
+       s.peek();
+       assertEquals(3, (int)s.size());
+       s.pop();
+       assertEquals(2, (int)s.size());
+       s.pop();
+       s.pop();
+       assertEquals(0, (int)s.size());
+       assertThrows(StackUnderflowException.class, () -> s.pop());
+   }
+    
+//    @Test
+//    public void testPush() {
+//       Ics124Stack s = new StackUsingList();
+//       assertThrows(Exception.class, () -> s.push(true));
+//       assertThrows(Exception.class, () -> s.push("string"));
+//       Object o = new Object();
+//       assertThrows(Exception.class, () -> s.push(o));
+//       s.push(123);
+//       assertEquals(123, (int)s.peek());
+//       assertEquals(123, (int)s.pop());       
+//   }
+    
+    @Test
+    public void testPop() {
+       Ics124Stack s = new StackUsingList();
+       assertThrows(StackUnderflowException.class, () -> s.pop());
+       s.push(123);
+       assertEquals(123, (int)s.pop());
+       assertThrows(StackUnderflowException.class, () -> s.pop());
+   }
+   
+   @Test
+   public void testIsEmpty(){
+       Ics124Stack s = new StackUsingList();
+       assertTrue(s.isEmpty());
+       s.push(123);
+       assertFalse(s.isEmpty());
+       s.peek();
+       assertFalse(s.isEmpty());
+       s.pop();
+       assertTrue(s.isEmpty());       
+   }
 }
--- test/ics124/c0350261/assignment3/TestQueue.java
+++ test/ics124/c0350261/assignment3/TestQueue.java
@@ -4,11 +4,107 @@
  */
 
 package ics124.c0350261.assignment3;
+import ics124.assignment3.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
  * @author Mariah Malczewska
  */
 public class TestQueue {
+  
+   @Test
+   public void testInitialState(){
+       Ics124Queue s = new Ics124Queue();
+       assertTrue(s.isEmpty());
+       assertFalse(s.isFull());
+       assertEquals(0, (int)s.size());
+       assertThrows(IndexOutOfBoundsException.class, () -> s.first());
+       assertThrows(IndexOutOfBoundsException.class, () -> s.dequeue());
 
+   }
+   
+   @Test
+   public void testOrder(){
+       Ics124Queue s = new Ics124Queue();
+       
+       s.enqueue(123);
+       s.enqueue(456);
+       s.enqueue(789);
+       assertEquals(789, (int)s.first());
+       assertEquals(789, (int)s.dequeue());
+       s.enqueue(159);
+       s.enqueue(48);
+       assertEquals(48, (int)s.first());
+       assertEquals(48, (int)s.dequeue());
+       assertEquals(159, (int)s.first());
+       assertEquals(159, (int)s.dequeue());
+       assertEquals(456, (int)s.first());
+       assertEquals(456, (int)s.dequeue());
+       assertEquals(123, (int)s.first());
+       assertEquals(123, (int)s.dequeue());
+   }
+   
+   @Test
+   public void testSize() {
+       Ics124Queue s = new Ics124Queue();
+       
+       assertEquals(0, (int) s.size());
+       s.isEmpty();
+       s.isFull();
+       assertEquals(0, (int) s.size());       
+       s.enqueue(123);
+       s.enqueue(456);
+       s.enqueue(789);
+       assertEquals(3, (int)s.size());
+       s.first();
+       assertEquals(3, (int)s.size());
+       s.dequeue();
+       assertEquals(2, (int)s.size());
+       s.dequeue();
+       s.dequeue();
+       assertEquals(0, (int)s.size());
+       assertThrows(IndexOutOfBoundsException.class, () -> s.dequeue());
+   }
+   
+//   @Test
+//   public void testEnqueue() {
+//       System.out.println("ENQUEUE TEST");
+//       Ics124Queue<Integer> s = new Ics124Queue<>();
+//       
+//       assertThrows(Exception.class, () -> s.enqueue(true));
+//       System.out.println("BOOL done");
+//       assertThrows(Exception.class, () -> s.enqueue("string"));
+//       System.out.println("STRING done");
+//       Object o = new Object();
+//       assertThrows(Exception.class, () -> s.enqueue(o));
+//       System.out.println("obj done");
+//       s.enqueue(123);
+//       assertEquals(123, (int)s.first());
+//       assertEquals(123, (int)s.dequeue());       
+//   }
+   
+   @Test
+   public void testDequeue() {
+       Ics124Queue s = new Ics124Queue();
+       
+       assertThrows(IndexOutOfBoundsException.class, () -> s.dequeue());
+       s.enqueue(123);
+       assertEquals(123, (int)s.dequeue());
+       assertThrows(IndexOutOfBoundsException.class, () -> s.dequeue());
+   }
+   
+   @Test
+   public void testIsEmpty(){
+       Ics124Queue s = new Ics124Queue();
+       
+       assertTrue(s.isEmpty());
+       s.enqueue(123);
+       assertFalse(s.isEmpty());
+       s.first();
+       assertFalse(s.isEmpty());
+       s.dequeue();
+       assertTrue(s.isEmpty());       
+   }   
 }
